3e055ee6d2cd29074878fc8fcc833d01c645aa30
Minh Duc <minhduc.gem08@gmail.com>
2018-12-03 02:09:19 +0700

    Add ServerControl

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ var admin = require("firebase-admin");
 
 var record_index = 0;
 
-var now = new Date();
-
 var serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
@@ -19,6 +17,7 @@ admin.initializeApp({
 
 var db = admin.database().ref();
 const numberRecordsRef = db.child("index");
+const remoteRef = db.child("remote");
 
 //here we start mosca
 var server = new mosca.Server(settings);
@@ -28,12 +27,21 @@ server.on('ready', setup);
 // fired when the mqtt server is ready
 function setup() {
   console.log('Mosca server is up and running');
-  numberRecordsRef.on("value",  function(snapshot){
+  numberRecordsRef.once("value",  function(snapshot){
     if(isNaN(snapshot.val())) record_index = 0;
     else record_index = Number(snapshot.val());
   });
 }
- 
+
+remoteRef.on("value", function(snapshot) {
+  var message = {
+    topic: "ServerControl",
+    payload: snapshot.val()
+  };
+  
+  server.publish(message, function(){});
+});
+
 // fired when a client is connected
 server.on('clientConnected', function(client) {
   console.log('client connected', client.id);
@@ -41,7 +49,7 @@ server.on('clientConnected', function(client) {
  
 // fired when a message is received
 server.on('published', function(packet, client) {
-  now = new Date();
+  var now = new Date();
 
   if(record_index == null) record_index = 0;
 
@@ -71,7 +79,7 @@ server.on('published', function(packet, client) {
     record_index++;
   }
 
-  console.log('Published : ', packet.topic, ' || ', packet.payload.toString());
+  console.log('Received : ', packet.topic, ' || ', packet.payload.toString());
 });
  
 // fired when a client subscribes to a topic
@@ -79,7 +87,7 @@ server.on('subscribed', function(topic, client) {
   console.log('subscribed : ', topic);
 });
  
-// fired when a client subscribes to a topic
+// fired when a client unsubscribes to a topic
 server.on('unsubscribed', function(topic, client) {
   console.log('unsubscribed : ', topic);
 });
